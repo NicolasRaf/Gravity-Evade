518b71928b27c4ee82de81f9322990cbd8289b53
Enzo Melo <enzomelo333@gmail.com>
2024-06-04 00:27:42 -0300

    World foi incrementado com um solo básico,para clarear e desenvolver ideias.Player foi criado com animações e pause menu,main menu e game over menu foram feitos.Verificar quais designs usados serão mantidos e quais são temporários,além de ajustar resolução de enquadramento das telas de menu

--- Scripts/Global.gd
+++ Scripts/Global.gd
@@ -1,15 +1,3 @@
 extends Node
 
-onready var transition_scene : PackedScene = preload("res://Scenes/FadeInTrasition.tscn")
 
-#Função que transiciona as cenas
-func transition_to_scene(destiny_scene: String) -> void:
-	var trans = transition_scene.instantiate()
-	trans.destiny_scene = destiny_scene
-	add_child(trans)
-
-## Cenas principais
-var main_scenes := {
-	"game" : preload("res://Scenes/World.tscn"),
-	"main_menu" : preload("res://Scenes/Main_Menu.tscn")
-}
--- Scripts/Pause_Menu.gd
+++ Scripts/Pause_Menu.gd
@@ -1,14 +1,20 @@
 extends CanvasLayer
 
-onready var continue_btn : Button = $Menu/continue_btn
+onready var ContinueBtn : Button = $ContinueBtn
 
 func _ready():
 	visible = false
 	
 func _unhandled_input(event):
-	if event.is_action_pressed("ui_cancel") and Global.can_pause:
+	if event.is_action_pressed("ui_cancel"):
 		visible = !visible
 		get_tree().paused = visible
-		continue_btn.grab_focus()
-		
+		ContinueBtn.grab_focus()
 
+
+func _on_ContinueBtn_pressed():
+	get_tree().paused = false
+	visible = false
+	
+func _on_ExitBtn_pressed():
+	get_tree().change_scene("res://Scenes/Main_Menu.tscn")
