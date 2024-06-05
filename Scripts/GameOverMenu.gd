eed72dc080bc94f701cd846357c97072a3b5f7f7
Nicolas Alves <nicolasalves136@gmail.com>
2024-06-05 10:37:08 -0300

    Ajustando caminhos bugados

--- Scripts/GameOverMenu.gd
+++
@@ -1,8 +0,0 @@
-extends Control
-
-
-func _on_Restart_pressed():
-	get_tree().change_scene("res://Scenes/World.tscn")
-
-func _on_Exit_pressed():
-	get_tree().change_scene("res://Scenes/Main_Menu.tscn")
--- Scripts/Main_Menu.gd
+++
@@ -1,7 +0,0 @@
-extends Control
-
-func _on_Start_button_pressed():
-	get_tree().change_scene("res://Scenes/World.tscn")
-	
-func _on_Exit_button_pressed():
-	get_tree().quit()
