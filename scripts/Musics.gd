61b8ad721125d91e8bcf40780bcaf75c95410122
NicolasRaf <nicolasalves136@gmail.com>
2024-06-21 05:46:41 -0300

    fix: Music not in loop

--- scripts/Musics.gd
+++ scripts/Musics.gd
@@ -1,12 +1,12 @@
 extends Node
 
 onready var musicPlayer = get_node("MusicPlayer")
-var musicNum : int = 3
+var musicNum : int = 1
 
 var musicDatabase : Dictionary = {
 1: preload("res://assets/Songs/\'Defense Matrix\' by Vyra 🇧🇪 _ Retro Music (No Copyright) 📍 (128).mp3"),
-2: preload("res://assets/Songs/🦾 Free Synthwave Music (For Videos) - _Voyager 1_ by John Tasoulas 🇬🇷 (128).mp3"),
-3: preload("res://assets/Songs/Waterflame - Time Machine (256).mp3")
+2:preload("res://assets/Songs/🦾 Free Synthwave Music (For Videos) - _Voyager 1_ by John Tasoulas 🇬🇷 (128).mp3"),
+3:preload("res://assets/Songs/Waterflame - Time Machine (256).mp3")
 }
 
 func _ready():
@@ -18,9 +18,12 @@ func playMusic() -> void:
 	musicPlayer.play()
 
 func _on_MusicPlayer_finished():
-	musicNum += 1
+	if musicNum >= musicDatabase.size():
+		musicNum = 1
+	else:
+		musicNum += 1
+		
 	musicPlayer.stream = musicDatabase.get(musicNum)
 	playMusic()
-	print(musicPlayer.stream)
-	if musicNum == musicDatabase.size():
-		musicNum = 1
+	print(musicNum >= musicDatabase.size())
+
